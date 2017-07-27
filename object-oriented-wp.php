7cd8f0c1dafb24b0dbbb1b4ce16ea6e2e41526b9
EvanWashkow <epwashkow@gmail.com>
2017-07-26 22:39:50 -0700

    Register plugin

---
+++ object-oriented-wp.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Object-Oriented WordPress
+Description: Access WordPress in a modern, object-oriented way
+Version:     0.0.1
+Author:      Evan Washkow
+Author URI:  https://github.com/EvanWashkow
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+?>
