08f5ae0938953c5a1499876b4aa64559facd8005
EvanWashkow <epwashkow@gmail.com>
2018-02-12 21:48:27 -0800

    Rebuild using main WordPress controller to do so

--- src/WordPress.php
+++ src/WordPress.php
@@ -1,5 +1,19 @@
 <?php
-class WordPress {
-    
+
+/**
+ * Root WordPress class
+ */
+class WordPress
+{
+    /**
+     * Start the object-oriented WordPress controller
+     */
+    final public static function Initialize()
+    {
+        $directory = __DIR__ . '/WordPress';
+        require_once( "{$directory}/Libraries.php" );
+        require_once( "{$directory}/Sites.php" );
+    }
 }
+WordPress::Initialize();
 ?>
