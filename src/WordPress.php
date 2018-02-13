acac864c1243bb1a1d44e60876b0b89edb004c40
EvanWashkow <epwashkow@gmail.com>
2018-02-12 22:09:54 -0800

    Remove WordPress Initialize()

--- src/WordPress.php
+++ src/WordPress.php
@@ -5,15 +5,5 @@
  */
 class WordPress
 {
-    /**
-     * Start the object-oriented WordPress controller
-     */
-    final public static function Initialize()
-    {
-        $directory = __DIR__ . '/WordPress';
-        require_once( "{$directory}/Libraries.php" );
-        require_once( "{$directory}/Sites.php" );
-    }
 }
-WordPress::Initialize();
 ?>
