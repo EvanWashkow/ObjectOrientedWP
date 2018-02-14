9574e1f3f8c7a7ca9aa00c976093590108ac70fd
EvanWashkow <epwashkow@gmail.com>
2018-02-14 11:35:00 -0800

    Document WordPress TimeZone library

--- src/WordPress/libraries/TimeZone.php
+++ src/WordPress/libraries/TimeZone.php
@@ -1,25 +1,69 @@
 <?php
 namespace WordPress;
 
-class TimeZone extends \DateTimeZone {
+/**
+ * Defines a time zone, compatible with WordPress's numeric GMT format
+ */
+class TimeZone extends \DateTimeZone
+{
     
-    //
-    // CONSTANTS
+    /***************************************************************************
+    *                               CONSTANTS
+    ***************************************************************************/
     
-    // Identify the type of timezone given
-    const GMT_TYPE            = 1;  // '+00:00'
-    const ABBREVIATION_TYPE   = 2;  // 'PST'
-    const IDENTIFICATION_TYPE = 3;  // 'America/Los_Angeles'
+    /**
+     * Identifies GMT types (+00:00)
+     *
+     * @var int
+     */
+    const GMT_TYPE = 1;
+    
+    /**
+     * Identifies abbreviated types ('PST')
+     *
+     * @var int
+     */
+    const ABBREVIATION_TYPE = 2;
+    
+    /**
+     * Identifies fully-specified time zones ('America/Los_Angeles')
+     *
+     * @var int
+     */
+    const IDENTIFICATION_TYPE = 3;
     
     
-    //
-    // INSTANCE
+    /***************************************************************************
+    *                           INSTANCE PROPERTIES
+    ***************************************************************************/
     
-    // Variables
+    /**
+     * Timezone data ( '+00:00' / 'PST' / 'America/Los_Angeles' )
+     *
+     * @var string
+     */
     protected $timezone;
+    
+    /**
+     * Timezone type (see constants)
+     *
+     * @var int
+     */
     protected $timezone_type;
     
-    // Create new TimeZone
+    /**
+     * DateTime instance used to convert this time zone to other formats
+     *
+     * @var \DateTime
+     */
+    private $dateTime;
+    
+    
+    /**
+     * Create new Time Zone instance
+     *
+     * @param mixed $mixed Time zone specifier ( '+00:00' / 'PST' / 'America/Los_Angeles' / WordPress numeric format )
+     */
     public function __construct( $mixed )
     {
         // Compensate for WordPress' decimal timezones (ex: 0, -1, 1.5)
@@ -78,43 +122,67 @@ class TimeZone extends \DateTimeZone {
     }
     
     
-    //
-    // CONVERSION -- cascades by order
+    /***************************************************************************
+    *                               CONVERSION
+    ***************************************************************************/
     
-    // Convert to Identifier (America/Los_Angeles)
-    public function toIdentifier( $fallback = true ) {
+    /**
+     * Try to convert to a time zone identifier ('America/Los_Angeles')
+     *
+     * @param bool $fallback If unable to convert to this format, try the next in line
+     * @return string|null
+     */
+    public function toIdentifier( bool $fallback = true )
+    {
+        $format = null;
         if ( self::IDENTIFICATION_TYPE == $this->timezone_type ) {
-            return $this->format( 'e' );
+            $format = $this->format( 'e' );
         }
         elseif ( $fallback ) {
-            return $this->toAbbreviation();
+            $format = $this->toAbbreviation();
         }
-        else {
-            return NULL;
-        }
+        return $format;
     }
     
-    // Convert to Abbreviation (PST)
-    public function toAbbreviation( $fallback = true ) {
+    
+    /**
+     * Try to convert to a time zone abbreviation ('PST')
+     *
+     * @param bool $fallback If unable to convert to this format, try the next in line
+     * @return string|null
+     */
+    public function toAbbreviation( bool $fallback = true )
+    {
+        $format = null;
         if ( self::ABBREVIATION_TYPE <= $this->timezone_type ) {
-            return $this->format( 'T' );
+            $format = $this->format( 'T' );
         }
         elseif ( $fallback ) {
-            return $this->toGMT();
+            $format = $this->toGMT();
         }
-        else {
-            return NULL;
-        }
+        return $format;
     }
     
-    // Convert to GMT (+00:00)
-    public function toGMT( $fallback = true ) {
+    
+    /**
+     * Convert to a time zone GMT offset ('+00:00')
+     *
+     * @return string
+     */
+    public function toGMT() {
         return $this->format( 'P' );
     }
     
-    // Convert to given format
-    private $dateTime;
-    private function format( $format ) {
+    /**
+     * Convert this time zone to the given format string
+     *
+     * @see http://php.net/manual/en/function.date.php
+     *
+     * @param string $format The PHP date formatting string
+     * @return string
+     */
+    private function format( string $format )
+    {
         if ( !isset( $this->dateTime )) {
             $this->dateTime = new \DateTime();
             $this->dateTime->setTimeZone( $this );
