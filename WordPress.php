e78de8f5ce862d30f31f5e43568f1a2cd3c77543
EvanWashkow <epwashkow@gmail.com>
2018-02-16 14:25:04 -0800

    Revert "Add `load.php` to load the main WordPress build"
    This reverts commit 931c7c318dda4f03c069d61655a64f20297b8648.

--- WordPress.php
+++ WordPress.php
@@ -1,5 +1,9 @@
 <?php
-// IMPORTANT: if building locally, include 'load.php'
+
+// Load composer's autoloader if on a local build
+if ( file_exists( __DIR__ . '/vendor/autoload.php' )) {
+    require_once( __DIR__ . '/vendor/autoload.php' );
+}
 
 /**
  * Root WordPress class
