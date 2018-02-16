83ddb83b67b6d0ca5c9029bae03f6f8aca387277
EvanWashkow <epwashkow@gmail.com>
2018-02-16 12:44:15 -0800

    Bug Fix: prevent double-loading (via composer or other plugins)

--- WordPress.php
+++ WordPress.php
@@ -1,5 +1,10 @@
 <?php
 
+// Exit. The class 'WordPress' already exists. This build may be corrupt.
+if ( class_exists( 'WordPress' )) {
+    return;
+}
+
 // Load composer's autoloader if on a local build
 if ( file_exists( __DIR__ . '/vendor/autoload.php' )) {
     require_once( __DIR__ . '/vendor/autoload.php' );
